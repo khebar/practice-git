95c086d8a6e144d349e7b5e6bf9ddae408d145c8
Ali Mahdian <96617826+khebar@users.noreply.github.com>
2025-04-18 16:40:32 +0330

    First Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void printHello() {
+    cout << "Hello, World" << endl;
+}
+
+void printStudentNumber() {
+    cout << "Student Number: 4033613075" << endl;
+}
+
+int main() {
+    printHello();
+    printStudentNumber();
+    return 0;
+}
